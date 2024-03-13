b33922850a0c22add7a80af9d6a1fe17b3ac84e5
Naveen Singh <101163510+naveensynergy7@users.noreply.github.com>
2024-03-13 11:56:58 +0530

    favicon change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,35 +10,18 @@ import Footer from "@/common/Footer";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta charset="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,shrink-to-fit=no"
-        />
-        <meta name="robots" content="index, follow" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer/>
+    <Head>
+    <meta charset="utf-8" />
+    <meta
+    name="viewport"
+    content="width=device-width,initial-scale=1,shrink-to-fit=no"
+    />
+    <meta name="robots" content="index, follow" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    </Head>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer/>
     </>
-  );
-}
+    );
+  }
--- src/pages/heic-to-jpg/index.js
+++ src/pages/heic-to-jpg/index.js
@@ -6,11 +6,11 @@ const App = () => {
     <>
       <Head>
         <title>heic to png image convert in 1 second</title>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta
           name="description"
           content="convert heic to png in 1 second | No file size limit | No images upload limit "
         />
+        <link rel="canonical" href="https://jpgtoopng.com/heic-to-jpg" />
         <meta
           name="keywords"
           content="heic to png, convert heic to png, heic to png multiple images at once, online, tool"
